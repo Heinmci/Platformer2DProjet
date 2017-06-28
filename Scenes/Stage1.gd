98ac264ee54ab92c3653a3410a3f33dcccb6f148
Jules <dejardin@hotmail.co.uk>
2017-06-28 14:54:05 +0200

    Inventory

--- Scenes/Stage1.gd
+++ Scenes/Stage1.gd
@@ -3,7 +3,28 @@ extends Node2D
 var jetons_total = 0
 var jetons_pris = 0
 
+var popup_shown = false
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	set_process(true)
 	pass
+	
+func _process(delta):
+	print("test")
+	if (Input.is_action_pressed("i_key")):
+		if (!popup_shown):
+			print("Toggled false")
+			var scene = load("res://Scenes/Inventory.tscn")
+			var node = scene.instance()
+			node.set_pos(get_pos())
+			get_parent().add_child(node)
+			get_parent().get_node("Inventory").popup_centered()
+			popup_shown = true
+		else:
+			print("Toggled true")
+			get_parent().get_node("Inventory").queue_free()
+			popup_shown = false
+		#get_tree().change_scene("res://Scenes/Inventory.tscn")
+
