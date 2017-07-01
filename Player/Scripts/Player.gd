ef560ae3346f3b37ae86d303daa18659a34dd906
Quentin <quentin.fougereau@gmail.com>
2017-07-01 13:48:50 +0200

    Animation player 'walk' + the player change siding

--- Player/Scripts/Player.gd
+++ Player/Scripts/Player.gd
@@ -7,54 +7,72 @@ var grounded = false
 var can_jump = false
 var jump_time = 0
 const TOP_JUMP_TIME = 0.1 # in seconds
+
+var anim = ""
+var siding_left = false
  
 # Start
 func _ready():
     # Set player properties
     acceleration = 1000
-    top_move_speed = 200
+    top_move_speed = 150
     top_jump_speed = 400
  
 # Apply force
 func apply_force(state):
+
+    var new_anim = anim
+    var new_siding_left = siding_left
+
     # Move Left
     if(Input.is_action_pressed("ui_left")):
         directional_force += DIRECTION.LEFT
+        new_anim = "run"
+        new_siding_left = true
      
     # Move Right
     if(Input.is_action_pressed("ui_right")):
         directional_force += DIRECTION.RIGHT
+        new_anim = "run"
+        new_siding_left = false
      
     # Jump
     if(Input.is_action_pressed("ui_select") && grounded):
         directional_force += DIRECTION.UP
+        new_anim = "stop"
 
-    # Jump
-#    if(Input.is_action_pressed("ui_select") && jump_time < TOP_JUMP_TIME && can_jump):
-#        directional_force += DIRECTION.UP
-#        jump_time += state.get_step()
-#    elif(Input.is_action_just_released("ui_select")):
-#    else:
-#        can_jump = false # Prevents the player from jumping more than once while in air
-     
+         
+    # The player does nothing
+    if (!Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_select")):
+        new_anim = "stop"
+
     # While on the ground
     if(grounded):
         can_jump = true
         jump_time = 0
         
+    # Change animation
+    if (new_anim != "" && new_anim != anim):
+        if (new_anim != "stop"):
+            anim = new_anim
+            get_node("AnimationPlayer").play(anim)
+        else:
+            anim = new_anim
+            get_node("AnimationPlayer").stop()
+
+    # Change player side
+    if (new_siding_left != siding_left):
+        if (new_siding_left):
+            get_node("Sprite").set_scale(Vector2(-1, 1))
+        else:
+            get_node("Sprite").set_scale(Vector2(1, 1))
+        siding_left = new_siding_left
 
 func _on_ground_check_body_enter( body ):
     grounded = true
     print("on ground")
     #When player is grounded the friction has to be set to 1
     set_friction(1)
-    
-     # Get groups
-#    var groups = body.get_groups()
-#
-#    # If we are on a solid ground
-#    if(groups.has("solid")):
-#        grounded = true
 
 
 func _on_ground_check_body_exit( body ):
@@ -62,10 +80,3 @@ func _on_ground_check_body_exit( body ):
     print("on air")
     #When player is on air the friction has to be set to 0
     set_friction(0)
-
-    # Get groups
-#    var groups = body.get_groups()
-#     
-#    # If we are on a solid ground
-#    if(groups.has("solid")):
-#        grounded = false
