48cd497f253370cda6b2bd91eef71f24c84cbb78
champCAR <gagoudechris@gmail.com>
2017-07-08 22:41:16 +0100

    Changements dans level1 avec un Area pour la fin

--- Scenes/Stage1.gd
+++ Scenes/Stage1.gd
@@ -8,3 +8,9 @@ var coins_collected = 0
 func _ready():
 	pass
 
+
+
+func _on_Area2D_body_enter( body ):
+	if (body.get_name() == "Player"):
+		get_node("YOUWIN").show()
+		get_tree().change_scene("res//Scenes/Levels/Level2/Stage.tscn")
