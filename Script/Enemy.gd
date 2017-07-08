aecf2f377952156405a4fed651703188192213fc
champCAR <gagoudechris@gmail.com>
2017-07-08 15:27:09 +0100

    Changements pour le personnage ennemi

--- Script/Enemy.gd
+++ Script/Enemy.gd
@@ -15,7 +15,7 @@ var rc_right = null
 var WALK_SPEED = 50
 
 #var bullet_class = preload("res://bullet.gd")
-
+var bullet_class = ""
 
 func _die():
 	queue_free()
@@ -25,7 +25,7 @@ func _pre_explode():
 	# Stay there
 	clear_shapes()
 	set_mode(MODE_STATIC)
-	get_node("sound").play("explode")
+	#get_node("sound").play("explode")
 
 
 func _integrate_forces(s):
@@ -43,6 +43,17 @@ func _integrate_forces(s):
 			var cc = s.get_contact_collider_object(i)
 			var dp = s.get_contact_local_normal(i)
 			
+			if (cc):
+				if (cc extends bullet_class and not cc.disabled):
+					set_mode(MODE_RIGID)
+					state = STATE_DYING
+					#lv = s.get_contact_local_normal(i)*400
+					s.set_angular_velocity(sign(dp.x)*33.0)
+					set_friction(1)
+					cc.disable()
+					#get_node("sound").play("hit")
+					break
+			
 			if (dp.x > 0.9):
 				wall_side = 1.0
 			elif (dp.x < -0.9):
@@ -51,7 +62,9 @@ func _integrate_forces(s):
 		if (wall_side != 0 and wall_side != direction):
 			direction = -direction
 			get_node("sprite").set_scale(Vector2(-direction, 1))
-		
+		if (direction < 0 and not rc_left.is_colliding() and rc_right.is_colliding()):
+			direction = -direction
+			get_node("sprite").set_scale(Vector2(-direction, 1))
 		elif (direction > 0 and not rc_right.is_colliding() and rc_left.is_colliding()):
 			direction = -direction
 			get_node("sprite").set_scale(Vector2(-direction, 1))
