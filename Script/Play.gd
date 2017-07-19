82ca21a2fcdfac7aa49ec5bba9936fc8ef8111dc
champCAR <gagoudechris@gmail.com>
2017-07-19 11:22:30 +0100

    Finition level1

--- Script/Play.gd
+++ Script/Play.gd
@@ -2,7 +2,7 @@ extends TextureButton
 
 func pressed():
 	get_tree().change_scene("res://Scenes/Levels/LevelCentrale/Stage.tscn")
-	Globals.set("number_of_lifes",1)
+	Globals.set("number_of_lifes",2)
 	Globals.set("has_sword",false)
 	Globals.set("current_level",1)
 	Globals.set("Level1_apple",false)
